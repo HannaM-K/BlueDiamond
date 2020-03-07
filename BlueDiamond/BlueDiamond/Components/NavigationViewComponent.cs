5471808a51c2cb6e4653c569e466717fdf047701
HannaM-K <hania.maj1995@gmail.com>
2020-03-07 20:16:26 +0100

    Play with library references an add more view

---
+++ BlueDiamond/BlueDiamond/Components/NavigationViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using BlueDiamond.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueDiamond.Components
+{
+    public class NavigationViewComponent : ViewComponent
+    {
+        private DemoRepository repository;
+        public NavigationViewComponent(DemoRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+                .Select(p => p.CategoryID)
+                .Distinct()
+                .OrderBy(c => c));
+        }
+    }
+}
